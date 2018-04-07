4a7e68c2ac88462c132cfd86907990e723d967ab
m.steger@protonmail.com <neuburg5DFGH!>
2018-04-07 15:32:25 +0200

    init

--- GridSearchValidationSet.py
+++ GridSearchValidationSet.py
@@ -1,7 +1,6 @@
 __author__ = 'MSteger'
 
 import numpy as np
-import pandas as pd
 import operator
 from sklearn.base import BaseEstimator, TransformerMixin
 from sklearn.model_selection import ParameterGrid
@@ -45,15 +44,20 @@ class GridSearchValidationSet(BaseEstimator, TransformerMixin):
 if __name__ == '__main__':
     from sklearn.ensemble import RandomForestClassifier
     from sklearn.metrics import accuracy_score
+
     X, y = np.random.rand(1000,10), np.random.randint(2,size=1000)
     y = np.where(y > 0, 'one', 'zero')
     X_val, y_val = X[500:], y[500:]
+
     model = RandomForestClassifier()
     param_grid = {
         'n_estimators': [10, 100],
         'max_depth': [2, 10]
     }
+
     GS = GridSearchValidationSet(model = model, param_grid = param_grid, X_val = X_val, y_val = y_val, scorer = accuracy_score, postprocessing = None, verbose = 1)
     GS.fit(X = X[:500], y = y[:500])
-    print 'Result: {}\nModel: {}\nBest Score: {}'.format(GS.result_, GS.best_estimator_, GS.best_score_)
+
+    print '\nResult: {}\nModel: {}\nBest Score: {}'.format(GS.result_, GS.best_estimator_, GS.best_score_)
+
     print 'done'
